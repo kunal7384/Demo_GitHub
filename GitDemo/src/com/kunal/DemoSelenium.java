24ba95564d0cce11899fb913a8a379ea51547100
Admin <Admin@Admin-PC>
2018-04-03 23:09:23 +0530

    First commit

---
+++ GitDemo/src/com/kunal/DemoSelenium.java
@@ -0,0 +1,24 @@
+package com.kunal;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoSelenium {
+	
+	public static WebDriver driver;
+	
+	
+	public static void main(String[] args) {
+		
+		driver = new ChromeDriver();
+		
+		
+		driver.get("https://github.com/");
+	System.out.println(driver.getTitle());
+	  
+	  
+		 
+		
+	}
+
+}
